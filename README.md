a1d967050b06610a7e346c44e4be170361f06118
Dominik Bähr <baehr@nexus-netsoft.com>
2019-08-09 18:11:54 +0200

    * fixed wrong jenkins settings * updated readme

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
  #### Project setup
  
  ###### Docker setup
- _COMPOSE_PROJECT_NAME_ is configured in /docker/.env
- 
  ```
 $ cd docker
 
-$ docker volume create --name=<COMPOSE_PROJECT_NAME>-ardb-data
-$ docker volume create --name=<COMPOSE_PROJECT_NAME>-elastic-data
-$ docker volume create --name=<COMPOSE_PROJECT_NAME>-db-data
-$ docker volume create --name=<COMPOSE_PROJECT_NAME>-rabbitmq-data
+$ export COMPOSE_PROJECT_NAME=nxsacademy
+$ docker volume create --name=$COMPOSE_PROJECT_NAME-ardb-data
+$ docker volume create --name=$COMPOSE_PROJECT_NAME-elastic-data
+$ docker volume create --name=$COMPOSE_PROJECT_NAME-db-data
+$ docker volume create --name=$COMPOSE_PROJECT_NAME-rabbitmq-data
 
 $ docker-compose up -d
  ```
 
 ###### Spryker setup
-Connect to _app_ container
-- name is f.e. "<COMPOSE_PROJECT_NAME>\_app\_<random_id>"
-- tip: display all started containers `$ docker ps`
+Connect to **app** container: "nxsacademy\_app\_ID"
 
 ```
 $ docker exec -it <Container-Name> bash
--- current/config/Shared/config_local.dist.php
+++ current/config/Shared/config_local.dist.php
@@ -1,6 +1,7 @@
 <?php
 
 use Monolog\Logger;
+use Pyz\Shared\Scheduler\SchedulerConfig;
 use Spryker\Shared\Application\ApplicationConstants;
 use Spryker\Shared\Collector\CollectorConstants;
 use Spryker\Shared\Customer\CustomerConstants;
@@ -12,6 +13,9 @@ use Spryker\Shared\ProductManagement\ProductManagementConstants;
 use Spryker\Shared\Propel\PropelConstants;
 use Spryker\Shared\PropelQueryBuilder\PropelQueryBuilderConstants;
 use Spryker\Shared\RabbitMq\RabbitMqEnv;
+use Spryker\Shared\Scheduler\SchedulerConstants;
+use Spryker\Shared\SchedulerJenkins\SchedulerJenkinsConfig;
+use Spryker\Shared\SchedulerJenkins\SchedulerJenkinsConstants;
 use Spryker\Shared\Search\SearchConstants;
 use Spryker\Shared\Session\SessionConfig;
 use Spryker\Shared\Session\SessionConstants;
@@ -132,8 +136,15 @@ $config[RabbitMqEnv::RABBITMQ_CONNECTIONS] = [
 ];
 
 /** Jenkins **/
-$config[SetupConstants::JENKINS_BASE_URL] = 'http://' . getenv('JENKINS_HOST') . ':' . getenv('JENKINS_PORT') . '/';
-$config[SetupConstants::JENKINS_DIRECTORY] = '/var/jenkins_home';
+$config[SchedulerConstants::ENABLED_SCHEDULERS] = [
+    SchedulerConfig::SCHEDULER_JENKINS,
+];
+
+$config[SchedulerJenkinsConstants::JENKINS_CONFIGURATION] = [
+    SchedulerConfig::SCHEDULER_JENKINS => [
+        SchedulerJenkinsConfig::SCHEDULER_JENKINS_BASE_URL => 'http://' . getenv('JENKINS_HOST') . ':' . getenv('JENKINS_PORT') . '/'
+    ],
+];
 
 $config[LogConstants::LOG_LEVEL] = Logger::ERROR;
 
