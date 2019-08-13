8e3ab5b56033dae037eeb0d5b925d6d84140691a
Patrick Schönfeld <schoenfeld@nexus-netsoft.com>
2019-08-13 18:01:55 +0200

    + Bugfix zed hostname

--- current/config/Shared/config_default_US.php
+++ current/config/Shared/config_default_US.php
@@ -30,24 +30,6 @@ $config[ProductManagementConstants::BASE_URL_YVES] = $config[ApplicationConstant
 $config[NewsletterConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
 $config[CustomerConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
 
-// ---------- Zed host
-$config[ApplicationConstants::HOST_ZED] = sprintf('zed.us.%s.local', $domain);
-$config[ApplicationConstants::PORT_ZED] = '';
-$config[ApplicationConstants::PORT_SSL_ZED] = '';
-$config[ApplicationConstants::BASE_URL_ZED] = sprintf(
-    'http://%s%s',
-    $config[ApplicationConstants::HOST_ZED],
-    $config[ApplicationConstants::PORT_ZED]
-);
-$config[ApplicationConstants::BASE_URL_SSL_ZED] = sprintf(
-    'https://%s%s',
-    $config[ApplicationConstants::HOST_ZED],
-    $config[ApplicationConstants::PORT_SSL_ZED]
-);
-$config[ZedRequestConstants::HOST_ZED_API] = $config[ApplicationConstants::HOST_ZED];
-$config[ZedRequestConstants::BASE_URL_ZED_API] = $config[ApplicationConstants::BASE_URL_ZED];
-$config[ZedRequestConstants::BASE_URL_SSL_ZED_API] = $config[ApplicationConstants::BASE_URL_SSL_ZED];
-
 // ---------- Trusted hosts
 $config[ApplicationConstants::YVES_TRUSTED_HOSTS] = [
     $config[ApplicationConstants::HOST_YVES],
@@ -71,33 +53,5 @@ $config[EventConstants::LOG_FILE_PATH] = APPLICATION_ROOT_DIR . '/data/US/logs/a
 $config[GlueApplicationConstants::GLUE_APPLICATION_DOMAIN] = sprintf('http://glue.us.%s.local', $domain);
 $config[GlueApplicationConstants::GLUE_APPLICATION_CORS_ALLOW_ORIGIN] = sprintf('http://glue.us.%s.local', $domain);
 
-$config[RabbitMqEnv::RABBITMQ_CONNECTIONS] = [
-    'DE' => [
-        RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'DE-connection',
-        RabbitMqEnv::RABBITMQ_HOST => 'localhost',
-        RabbitMqEnv::RABBITMQ_PORT => '5672',
-        RabbitMqEnv::RABBITMQ_PASSWORD => 'mate20mg',
-        RabbitMqEnv::RABBITMQ_USERNAME => 'DE_development',
-        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => '/DE_development_zed',
-        RabbitMqEnv::RABBITMQ_STORE_NAMES => ['DE'],
-    ],
-    'AT' => [
-        RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'AT-connection',
-        RabbitMqEnv::RABBITMQ_HOST => 'localhost',
-        RabbitMqEnv::RABBITMQ_PORT => '5672',
-        RabbitMqEnv::RABBITMQ_PASSWORD => 'mate20mg',
-        RabbitMqEnv::RABBITMQ_USERNAME => 'AT_development',
-        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => '/AT_development_zed',
-        RabbitMqEnv::RABBITMQ_STORE_NAMES => ['AT'],
-    ],
-    'US' => [
-        RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'US-connection',
-        RabbitMqEnv::RABBITMQ_HOST => 'localhost',
-        RabbitMqEnv::RABBITMQ_PORT => '5672',
-        RabbitMqEnv::RABBITMQ_PASSWORD => 'mate20mg',
-        RabbitMqEnv::RABBITMQ_USERNAME => 'US_development',
-        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => '/US_development_zed',
-        RabbitMqEnv::RABBITMQ_STORE_NAMES => ['US'],
-        RabbitMqEnv::RABBITMQ_DEFAULT_CONNECTION => true,
-    ],
-];
+// ---------- RabbitMQ
+$config[RabbitMqEnv::RABBITMQ_API_VIRTUAL_HOST] = '/US_development_zed';
